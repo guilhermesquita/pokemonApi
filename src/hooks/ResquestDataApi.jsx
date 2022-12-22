03272de5632e42a63c5e1a90ce38cba46c54f68c
ELIAS RICARDO MESQUITA <guilhermer_mesquita@hotmail.com>
2022-12-21 21:24:21 -0400

    minimizando o código do useRequestDataApi

--- src/hooks/ResquestDataApi.jsx
+++ src/hooks/ResquestDataApi.jsx
@@ -7,18 +7,9 @@ export const useRequestDataApi = (id) => {
     const [img, setImg] = useState([{}])
 
     useEffect(()=>{
-        axios.get("https://pokeapi.co/api/v2/pokemon/")
-        .then((r)=>{
-            console.log(r.data.results)
-            setPokemons(r.data.results)
-        })
-        .catch((e)=>console.log(e), [])
-    })
-
-    useEffect(()=>{
         axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
         .then((r)=>{
-            //console.log(r.data.sprites.front_default)
+            setPokemons(r.data.results)
             setImg(r.data.sprites.other.home.front_default)
         })
         .catch((e)=>console.log(e), [])
