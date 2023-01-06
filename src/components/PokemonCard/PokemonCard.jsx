6fa23da36af52988dcdba3ee5c61b720507147d4
ELIAS RICARDO MESQUITA <guilhermer_mesquita@hotmail.com>
2023-01-06 15:12:05 -0400

    distribuindo cores pelos cards

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -4,6 +4,7 @@ import { GlobalContext } from "../../context/GlobalContext"
 import { Card, SectionMain, Grass, Poison, Fire, Flying, Water, Bug, Normal, PokemonPhoto, PokeballBack } from "./style"
 import { changeToDetails } from '../../routes/coordinator'
 import { ApiContext } from "../../context/ApiContext"
+import { CardBug, CardFire, CardGrass } from "./styleCards"
 import { useRequestDataApi } from '../../hooks/ResquestDataApi'
 import pokeballBack from '../../assets/pngwing.svg'
 import grass from '../../assets/vetorTypes/grass.svg'
@@ -35,45 +36,45 @@ export const PokemonCard = () => {
         if (skill.name === 'grass') {
             return (
                 <Grass>
-                    <img src={grass} /> 
+                    <img src={grass} />
                     {skill.name}
                 </Grass>)
         } else if (skill.name === 'poison') {
             return (
                 <Poison>
-                    <img src={poison}/>
+                    <img src={poison} />
                     {skill.name}
                 </Poison>)
         } else if (skill.name === 'fire') {
             return (
                 <Fire>
-                    <img src={fire}/> 
+                    <img src={fire} />
                     {skill.name}
                 </Fire>)
         } else if (skill.name === 'flying') {
             return (
                 <Flying>
-                    <img src={flying}/>
+                    <img src={flying} />
                     {skill.name}
                 </Flying>)
         }
         else if (skill.name === 'water') {
             return (
                 <Water>
-                    <img src={water}/>
+                    <img src={water} />
                     {skill.name}
                 </Water>)
         }
         else if (skill.name === 'bug') {
             return (
                 <Bug>
-                    <img src={bug}/> 
+                    <img src={bug} />
                     {skill.name}
                 </Bug>)
         } else if (skill.name === 'normal') {
             return (
                 <Normal>
-                    <img src={normal}/>
+                    <img src={normal} />
                     {skill.name}
                 </Normal>)
         }
@@ -83,71 +84,153 @@ export const PokemonCard = () => {
         if (skill.name === 'grass') {
             return (
                 <Grass>
-                    <img src={grass}/>
+                    <img src={grass} />
                     {skill.name}
                 </Grass>)
         } else if (skill.name === 'poison') {
             return (
                 <Poison>
-                    <img src={poison}/>
+                    <img src={poison} />
                     {skill.name}
                 </Poison>)
         } else if (skill.name === 'fire') {
             return (
                 <Fire>
-                    <img src={fire}/>
+                    <img src={fire} />
                     {skill.name}
                 </Fire>)
         } else if (skill.name === 'flying') {
             return (
                 <Flying>
-                    <img src={flying}/>
+                    <img src={flying} />
                     {skill.name}
                 </Flying>)
         }
         else if (skill.name === 'water') {
             return (
                 <Water>
-                    <img src={water}/>
+                    <img src={water} />
                     {skill.name}
                 </Water>)
         }
         else if (skill.name === 'bug') {
             return (
                 <Bug>
-                    <img src={bug}/> 
+                    <img src={bug} />
                     {skill.name}
                 </Bug>)
         } else if (skill.name === 'normal') {
             return (
                 <Normal>
-                    <img src={normal}/>
+                    <img src={normal} />
                     {skill.name}
                 </Normal>)
         }
     }
 
+    const renderCards = () => {
+        if (skills1.name === 'grass') {
+            return (
+                <CardGrass>
+                    <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
+                    <header>
+                        <span>#0{id}</span>
+                        <h2>{pokemon.name}</h2>
+                    </header>
 
+                    <article>
+                        {types1(skills1)}
+                        {types2(skills2)}
+                    </article>
+
+                    <article>
+                        <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
+                        <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
+                    </article>
+                </CardGrass>)
+        }
+        else if (skills1.name === 'fire') {
+            return (
+                <CardFire>
+                    <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
+                    <header>
+                        <span>#0{id}</span>
+                        <h2>{pokemon.name}</h2>
+                    </header>
+
+                    <article>
+                        {types1(skills1)}
+                        {types2(skills2)}
+                    </article>
+
+                    <article>
+                        <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
+                        <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
+                    </article>
+                </CardFire>)
+        } else if(skills1.name == 'bug'){
+            return (
+                <CardBug>
+                    <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
+                    <header>
+                        <span>#0{id}</span>
+                        <h2>{pokemon.name}</h2>
+                    </header>
+
+                    <article>
+                        {types1(skills1)}
+                        {types2(skills2)}
+                    </article>
+
+                    <article>
+                        <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
+                        <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
+                    </article>
+                </CardBug>)
+        } else if(skills1.name =='normal'){
+            return (
+                <CardFire>
+                    <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
+                    <header>
+                        <span>#0{id}</span>
+                        <h2>{pokemon.name}</h2>
+                    </header>
+
+                    <article>
+                        {types1(skills1)}
+                        {types2(skills2)}
+                    </article>
+
+                    <article>
+                        <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
+                        <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
+                    </article>
+                </CardFire>)
+        }else{
+            return (
+                <Card>
+                    <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
+                    <header>
+                        <span>#0{id}</span>
+                        <h2>{pokemon.name}</h2>
+                    </header>
+
+                    <article>
+                        {types1(skills1)}
+                        {types2(skills2)}
+                    </article>
+
+                    <article>
+                        <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
+                        <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
+                    </article>
+                </Card>)
+        }
+    }
     return (
         <SectionMain>
             <PokemonPhoto src={pokemonImgMain} width={'40%'} />
-            <Card>
-                <PokeballBack src={pokeballBack} height={'90%'} width={'65%'} />
-                <header>
-                    <span>#0{id}</span>
-                    <h2>{pokemon.name}</h2>
-                </header>
-
-                <article>
-                    {types1(skills1)}
-                    {types2(skills2)}
-                </article>
-
-                <article>
-                    <button onClick={() => changeToDetails(navigate, pokemon.name, setName, id, setIdPok)}>Detalhes</button>
-                    <button onClick={() => addToPokedex(pokemonList, id)}>Capturar!</button>
-                </article>
-            </Card>
+            {renderCards()}
         </SectionMain>
     )
 }
---
+++ src/components/PokemonCard/styleCards.js
@@ -0,0 +1,224 @@
+import styled from "styled-components";
+
+export const CardGrass = styled.section`
+    font-family: "Poppins", 'Courier New', Courier, monospace;
+    background-color: #729F92;
+    min-width: 30vw;
+    min-height: 25vh;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 15px;
+    padding-right: 30px;
+    gap: 14px;
+    padding-bottom: 10px;
+    padding-top: 12px;
+    header{
+        span{
+            font-size: 12px;
+            color: #FFFFFF;
+            font-weight: 700;
+        }
+        h2{
+            color: #FFFFFF;
+        }
+    }
+    article:nth-child(4){
+        margin-top: 20px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        button{height: 20px;}
+        button:nth-child(1){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 600;
+            border: none;
+            text-decoration: underline;
+            background-color: transparent;
+            color: white;
+        }
+        button:nth-child(2){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 500;
+            background-color: #FFFFFF;
+            text-align: center;
+            width: 40%;
+            height: 32px;
+            border: none;
+            border-radius: 5px;
+            :hover{
+                cursor: pointer;
+            }
+        }
+    }
+`
+
+export const CardFire = styled.section`
+    font-family: "Poppins", 'Courier New', Courier, monospace;
+    background-color: #EAAB7D;
+    min-width: 30vw;
+    min-height: 25vh;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 15px;
+    padding-right: 30px;
+    gap: 14px;
+    padding-bottom: 10px;
+    padding-top: 12px;
+    header{
+        span{
+            font-size: 12px;
+            color: #FFFFFF;
+            font-weight: 700;
+        }
+        h2{
+            color: #FFFFFF;
+        }
+    }
+    article:nth-child(4){
+        margin-top: 20px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        button{height: 20px;}
+        button:nth-child(1){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 600;
+            border: none;
+            text-decoration: underline;
+            background-color: transparent;
+            color: white;
+        }
+        button:nth-child(2){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 500;
+            background-color: #FFFFFF;
+            text-align: center;
+            width: 40%;
+            height: 32px;
+            border: none;
+            border-radius: 5px;
+            :hover{
+                cursor: pointer;
+            }
+        }
+    }
+`
+
+export const CardBug = styled.section`
+    font-family: "Poppins", 'Courier New', Courier, monospace;
+    background-color: #76A866;
+    min-width: 30vw;
+    min-height: 25vh;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 15px;
+    padding-right: 30px;
+    gap: 14px;
+    padding-bottom: 10px;
+    padding-top: 12px;
+    header{
+        span{
+            font-size: 12px;
+            color: #FFFFFF;
+            font-weight: 700;
+        }
+        h2{
+            color: #FFFFFF;
+        }
+    }
+    article:nth-child(4){
+        margin-top: 20px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        button{height: 20px;}
+        button:nth-child(1){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 600;
+            border: none;
+            text-decoration: underline;
+            background-color: transparent;
+            color: white;
+        }
+        button:nth-child(2){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 500;
+            background-color: #FFFFFF;
+            text-align: center;
+            width: 40%;
+            height: 32px;
+            border: none;
+            border-radius: 5px;
+            :hover{
+                cursor: pointer;
+            }
+        }
+    }
+`
+export const CardNormal = styled.section`
+    font-family: "Poppins", 'Courier New', Courier, monospace;
+    background-color: #BF9762;
+    min-width: 30vw;
+    min-height: 25vh;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding-left: 15px;
+    padding-right: 30px;
+    gap: 14px;
+    padding-bottom: 10px;
+    padding-top: 12px;
+    header{
+        span{
+            font-size: 12px;
+            color: #FFFFFF;
+            font-weight: 700;
+        }
+        h2{
+            color: #FFFFFF;
+        }
+    }
+    article:nth-child(4){
+        margin-top: 20px;
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        align-items: center;
+        button{height: 20px;}
+        button:nth-child(1){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 600;
+            border: none;
+            text-decoration: underline;
+            background-color: transparent;
+            color: white;
+        }
+        button:nth-child(2){
+            cursor: pointer;
+            font-family: "Poppins", 'Courier New', Courier, monospace;
+            font-weight: 500;
+            background-color: #FFFFFF;
+            text-align: center;
+            width: 40%;
+            height: 32px;
+            border: none;
+            border-radius: 5px;
+            :hover{
+                cursor: pointer;
+            }
+        }
+    }
+`
